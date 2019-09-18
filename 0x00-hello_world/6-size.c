bcc38983065f39622a81c9914a6c962f795ea144
student1043 <1043@holbertonschool.com>
2019-09-18 13:02:24 +0000

    Fix 20

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+/**
+ * main - Entry point
+ * print message
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("Size of a char: %2d \n", sizeof(char));
+  printf("Size of an int %2d \n", sizeof(int));
+  printf("Size of a long int: %2d \n", sizeof(long int));
+  printf("Size of a long long int: %2d \n", sizeof(float));
+  printf("Size of a float: %2d \n", sizeof(long int));
+  Return (0);
+}
