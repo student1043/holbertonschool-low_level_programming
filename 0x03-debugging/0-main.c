84631c134fdbce001efaa93190039e1ca8ac3c2d
student1043 <1043@holbertonschool.com>
2019-09-24 20:47:05 +0000

    Fix 03

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -11,6 +11,6 @@ int main(void)
 
         i = 98;
         positive_or_negative(i);
-
+	
         return (0);
 }
