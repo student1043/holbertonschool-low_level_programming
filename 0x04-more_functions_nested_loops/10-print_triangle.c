ab7fe1de6433553709e0d50403379ebd2c4464cf
student1043 <1043@holbertonschool.com>
2019-09-26 16:33:29 +0100

    Upload 07

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,6 +7,8 @@
 void print_triangle(int size)
 {
 int i, k;
+if (size > 0)
+{
 for (i = 0; i < size; i++)
 {
 for (k = 0; k < size; k++)
@@ -20,4 +22,7 @@ _putchar('#');
 }
 _putchar('\n');
 }
+}
+else
+_putchar('\n');
 }
