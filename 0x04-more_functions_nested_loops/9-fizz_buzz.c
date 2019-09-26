07cfe2e8d0a4796560ddffb3ae5a8029dd03046a
student1043 <1043@holbertonschool.com>
2019-09-26 16:45:40 +0100

    Upload 10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i < 101; i++)
+for (i = 1; i < 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
@@ -31,5 +31,5 @@ printf("%d", i);
 printf(" ");
 }
 }
-printf("\n");
+printf("Buzz\n");
 }
