a6aab5b067fa045a4e62dc61b68d38e9587d7eb3
student1043 <1043@holbertonschool.com>
2019-10-01 13:59:14 +0100

    Fix 12

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts2 - check the code for Holberton School students.
+ * @str: is string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+int i;
+int j;
+while (str[j] != '\0')
+{
+j++;
+}
+while (*str)
+{
+for (i = 0; i < j - 1; i += 2)
+_putchar(str[i]);
+break;
+}
+_putchar('\n');
+}
