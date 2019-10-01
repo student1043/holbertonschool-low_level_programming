87a5806f11dd38cadb6b838b31bfe3f4d87f8bde
student1043 <1043@holbertonschool.com>
2019-10-01 14:24:12 +0100

    Fix 14

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - check the code for Holberton School students.
+ * @str: string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+int i;
+int c = 0;
+while (str[c] != '\0')
+{
+c++;
+}
+if (c % 2 == 0)
+{
+for (i = c / 2; i < c; i++)
+_putchar(str[i]);
+}
+else if (c % 2 != 0)
+{
+for (i = (c - 1) / 2; i < c; i++)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
