ce9c1637cc0ec9b9dc49f16b45abd4647c812a00
student1043 <1043@holbertonschool.com>
2019-10-01 16:10:05 +0100

    Fixing

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - check the code for Holberton School students.
+ * @a: integer
+ * @n: integer
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+printf(", ");
+}
+printf("\n");
+}
