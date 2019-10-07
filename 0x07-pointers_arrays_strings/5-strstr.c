c77d83c35ff94f7948e7463c7e4b807eefde7371
student1043 <1043@holbertonschool.com>
2019-10-07 22:46:18 +0100

    Fixes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,14 +8,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-unsigned int i, j;
-for (i = 0; needle[i] != '\0'; i++)
+unsigned int i = 0, j = 0;
+
+while (haystack[i])
 {
-for (j = 0; haystack[j] != '\0'; j++)
+while (needle[j] && (haystack[i] == needle[0]))
 {
-if (needle[i] == haystack[j])
-return (needle);
+if (haystack[i + j] == needle[j])
+j++;
+else
+break;
 }
+if (needle[j])
+{
+i++;
+j = 0;
+}
+else
+return (haystack + i);
 }
 return (0);
 }
