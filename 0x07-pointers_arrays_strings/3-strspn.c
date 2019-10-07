dae8fc817a057aa16ddba225a7071b1260e4687b
student1043 <1043@holbertonschool.com>
2019-10-07 15:44:29 +0100

    Fixes

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i,j;
-i=0;
-while(*(s+i)){
-j=0;
-while(*(accept+j)){
-if(*(s+i) == *(accept+j)){
+unsigned int i, j;
+i = 0;
+while (*(s + i))
+{
+j = 0;
+while (*(accept + j))
+{
+if (*(s + i) == *(accept + j))
+{
 break;
 }
 j++;
 }
-if(!*(accept+j)){
-return i;
+if (!*(accept + j))
+{
+return (i);
 }
 i++;
 }
-return i;
+return (i);
 }
