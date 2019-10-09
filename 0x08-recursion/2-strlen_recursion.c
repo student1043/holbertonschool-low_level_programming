3868fc321af2fb65c0e87456a1211ec7abc958a1
student1043 <1043@holbertonschool.com>
2019-10-09 13:52:27 +0100

    Code for string length / Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: string
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+else
+{
+return (1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
