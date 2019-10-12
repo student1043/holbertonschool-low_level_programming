72b2e4f6d7adb9b93f738a163b10948f55b47ce5
student1043 <1043@holbertonschool.com>
2019-10-12 17:19:57 +0100

    Task #0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - writes the character c to stdout
+ * @argv: The character to print
+ * @argc: integer
+ * Return: On success 1.
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+}
