7730e273653885926c94118598f7f408ba2056ed
student1043 <1043@holbertonschool.com>
2019-10-21 20:10:37 +0100

    yes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,6 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #define dog_t struct dog
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
