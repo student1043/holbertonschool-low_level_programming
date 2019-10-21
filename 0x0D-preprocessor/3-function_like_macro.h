8738efc3124e0bc105485b806db706bd299f5025
student1043 <1043@holbertonschool.com>
2019-10-21 17:47:14 +0100

    Fixing

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_ABS
-#define FUNCTION_ABS
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
 #define ABS(x) ((x < 0) ? (-x) : (x))
 
-#endif /* FUNCTION_ABS */
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
