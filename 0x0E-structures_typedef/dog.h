454bf0ac3f7e0910d8b10e80eb3555ad964e39c6
student1043 <1043@holbertonschool.com>
2019-10-21 20:12:50 +0100

    Fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,15 +9,15 @@
  *
  * Description: Longer description
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#define dog_t struct dog
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
