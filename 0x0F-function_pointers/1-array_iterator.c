9a71daf923cb4866216d7f914ad588145d3cefa0
student1043 <1043@holbertonschool.com>
2019-10-23 19:37:59 +0100

    Nice Push

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints a name as is
+ * @array: name of the person
+ * @size: size of the array
+ * @action: action done on array;
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - Struct op
+ * @a: The operator
+ * @b: The function associated
+ * Return: Addition of a and b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - Struct op
+ * @a: The operator
+ * @b: The function associated
+ * Return: Nothing
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - Struct op
+ * @a: The operator
+ * @b: The function associated
+ * Return: Nothing
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - Struct op
+ * @a: The operator
+ * @b: The function associated
+ * Return: Nothing
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - Struct op
+ * @a: The operator
+ * @b: The function associated
+ * Return: Nothing
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
