8fe703cf35758fc8c47cd3184440a0bc90622345
student1043 <1043@holbertonschool.com>
2019-10-31 17:44:57 +0100

    Pushing Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
 /**
  * print_list - writes the character c to stdout
  * @head: New List
@@ -9,5 +11,16 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-
+list_t *new_node;
+head = malloc(sizeof(list_t));
+new_node = malloc(sizeof(list_t));
+if (head)
+{
+new_node->str = strdup(str);
+new_node->next = *head;
+*head = new_node;
+}
+else
+return (NULL);
+return (*head);
 }
