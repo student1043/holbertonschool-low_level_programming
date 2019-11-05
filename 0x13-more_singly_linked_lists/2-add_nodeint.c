859971df74b06d85ff4a8bdaa77d21fd397ebd14
student1043 <1043@holbertonschool.com>
2019-11-05 13:24:27 +0100

    Pushing Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,29 +4,18 @@
 #include "lists.h"
 
 /**
- * print_listint - check the code for Holberton School students.
- * @h: list
+ * add_nodeint - check the code for Holberton School students.
+ * @head: list
+ * @n: constant
  * Return: Always 0.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *new_node;
 new_node = malloc(sizeof(listint_t));
-if (n == '\0')
-{
-return (NULL);
-}
 if (new_node == NULL)
-{
 return (NULL);
-}
 new_node->n = n;
-if (new_node->n == '\0')
-{
-free(new_node);
-return (NULL);
-}
-while (new_node->next)
 new_node->next = *head;
 *head = new_node;
 return (new_node);
