d244a403ebb804e374b129abd86c1a5cb6b027c3
student1043 <1043@holbertonschool.com>
2019-11-05 14:09:40 +0100

    Fixing Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,16 +4,16 @@
 #include "lists.h"
 
 /**
- * free_listint - check the code for Holberton School students.
+ * free_listint2 - check the code for Holberton School students.
  * @head: list
  * Return: Always 0.
  */
 void free_listint2(listint_t **head)
 {
 listint_t *c;
-if (*head == NULL)
+if (head == NULL)
 return;
-while(*head)
+while (*head)
 {
 c = (*head)->next;
 free(*head);
