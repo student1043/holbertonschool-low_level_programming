255fb8c055291535a1ad99cac71beff9f35a2475
student1043 <1043@holbertonschool.com>
2019-11-05 14:50:28 +0100

    Fixing Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,6 +6,7 @@
 /**
  * get_nodeint_at_index - check the code for Holberton School students.
  * @head: list
+ * @index: index of the node
  * Return: Always 0.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -13,12 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int i;
 listint_t *temp;
 temp = head;
+if (temp == NULL)
+return (NULL);
 for (i = 0; i < index; i++)
 {
 temp = temp->next;
 }
-if (temp == NULL)
-return (NULL);
-
 return (temp);
 }
