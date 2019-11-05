45df43616ee1c2c5a15cebd1962dc4b76ef36ebf
student1043 <1043@holbertonschool.com>
2019-11-05 14:48:48 +0100

    Pushing Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - check the code for Holberton School students.
+ * @head: list
+ * Return: Always 0.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int i;
+listint_t *temp;
+temp = head;
+for (i = 0; i < index; i++)
+{
+temp = temp->next;
+}
+if (temp == NULL)
+return (NULL);
+
+return (temp);
+}
