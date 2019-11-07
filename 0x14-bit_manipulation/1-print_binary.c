74ebc3f4662f6b5811fa7c36797623938630bfb2
student1043 <1043@holbertonschool.com>
2019-11-07 12:42:36 +0100

    Pushing Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * print_binary - check the code for Holberton School students.
+ * @n: is const char
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+int c;
+unsigned int k;
+for (c = 31; c >= 0; c--)
+{
+k = n >> c;
+if (k & 1)
+_putchar('1');
+else
+_putchar('0');
+}
+}
