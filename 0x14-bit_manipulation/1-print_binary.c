c701e418406886ad048b4a530097854b36c79e88
student1043 <1043@holbertonschool.com>
2019-11-07 20:58:27 +0100

    Fixing Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,23 @@
  */
 void print_binary(unsigned long int n)
 {
-if (n > 1)
-print_binary(n >> 1);
-printf("%ld", n & 1);
+int k, i;
+if (n < 2)
+{
+_putchar(n + '0');
+}
+else
+{
+for (i = 31; i >= 0; i--)
+{
+k = n >> i;
+if (k != 0)
+{
+if (k & 1)
+_putchar('1');
+else
+_putchar('0');
+}
+}
+}
 }
