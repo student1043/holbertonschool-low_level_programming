6d764201f68db8cfc43c3e6dbcdbca0396e142e2
student1043 <1043@holbertonschool.com>
2019-11-07 13:36:57 +0100

    Pushing Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * set_bit - check the code for Holberton School students.
+ * @n: is the integer
+ * @index: is index of the given bit
+ * Return: Always 0.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 31)
+return (-1);
+*n |= 1UL << index;
+return (1);
+}
