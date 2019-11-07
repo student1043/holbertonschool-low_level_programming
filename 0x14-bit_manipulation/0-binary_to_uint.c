3e2a8d2e8440ae7898d9d85183977e1108b0252c
student1043 <1043@holbertonschool.com>
2019-11-07 12:21:45 +0100

    Pushing Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - check the code for Holberton School students.
+ * @b: is const char
+ * Return: Always 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int k = 1;
+unsigned int i = 0;
+int c;
+unsigned int len;
+
+len = strlen(b);
+
+for (c = len - 1; c >= 0; c--)
+{
+if (b[c] != 48 && b[c] != 49)
+return (0);
+if (b[c] == '1')
+{
+i += k;
+}
+k *= 2;
+}
+return (i);
+}
