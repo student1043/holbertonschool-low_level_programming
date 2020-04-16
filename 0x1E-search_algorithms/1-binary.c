4bc18d3266308709a2d186d9d1a5e508f611acd9
student1043 <1043@holbertonschool.com>
2020-04-16 22:18:22 +0100

    Fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,14 +13,14 @@ size_t i;
 int k;
 int low = array[0];
 int high = array[0];
-for (i = 0; i < size; i++)
+for (i = 0; i < size - 1; i++)
 {
 if (array[i] > high)
 {
 high = array[i];
 }
 }
-for (i = 0; i < size; i++)
+for (i = 0; i < size - 1; i++)
 {
 if (array[i] < low)
 {
@@ -30,12 +30,12 @@ low = array[i];
 printf("Searching in array: ");
 for (k = low; k <= high; k++)
 {
-printf("%d, ", array[i]);
 if (k == high)
 {
 printf("%d\n", array[i]);
 break;
 }
+printf("%d, ", array[i]);
 }
 while (low <= high)
 {
